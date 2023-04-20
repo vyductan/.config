2cb6a4310c6d024b20cde49d89054315321501a2
VyDucTan <vdt5snet@gmail.com>
2023-04-20 22:14:33 +0700

    feat(ui): add border for lsp hover, signatureHelp, diagnostic

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -12,3 +12,15 @@ opt.shell = "fish"
 opt.encoding = "utf-8"
 
 opt.whichwrap = "b,s,h,l,<,>,[,]"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "single",
+})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  border = "single",
+})
+vim.diagnostic.config({
+  float = {
+    border = "single",
+  },
+})

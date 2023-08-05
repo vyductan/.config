95f9105dbeff30ca6b0345817a60465f383f8ed9
VyDucTan <vdt5snet@gmail.com>
2023-08-05 12:09:43 +0700

    chore(nvim-editor): remove unused code

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,24 +1,12 @@
 return {
-  -- file explorer
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {},
-  },
   {
     "nvim-telescope/telescope.nvim",
     keys = {
+      -- search key shortcuts
       { "<leader><space>", false },
     },
     config = function(_)
       require("telescope").load_extension("diff")
     end,
-  },
-  {
-    "NvChad/nvim-colorizer.lua",
-    opts = {
-      user_default_options = {
-        tailwind = true,
-      },
-    },
   },
 }

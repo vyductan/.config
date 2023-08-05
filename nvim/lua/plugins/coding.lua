2fd700e412ad72faa8f7f55139592075d35e3de3
VyDucTan <vdt5snet@gmail.com>
2023-08-05 12:07:18 +0700

    feat(nvim-cmp): add border

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,13 +1,23 @@
 return {
+  -- TODO: show import source
+  -- https://github.com/hrsh7th/nvim-cmp/issues/98
   {
-    "echasnovski/mini.pairs",
-    enabled = false,
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      -- add border
+      opts.window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
+    end,
   },
-  {
-    "windwp/nvim-autopairs",
-    event = "VeryLazy",
-    opts = {},
-  },
+  -- {
+  --   "windwp/nvim-autopairs",
+  --   event = "VeryLazy",
+  --   opts = {},
+  -- },
   {
     "barrett-ruth/import-cost.nvim",
     build = "sh install.sh pnpm",

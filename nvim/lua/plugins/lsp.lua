3f1b761f06ba18661a708198cdb954ff74840b66
VyDucTan <vdt5snet@gmail.com>
2023-08-05 12:16:15 +0700

    chore(nvim-lsp): remove unused code

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,31 +4,7 @@ return {
     opts = {
       {
         automatic_installation = true,
-        -- ensure_installed = {
-        --   "tsserver",
-        --   "pylsp",
-        -- },
       },
     },
-  },
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = {
-  --     ensure_installed = {
-  --       "flake8",
-  --       "black",
-  --     },
-  --   },
-  -- },
-  {
-    "glepnir/lspsaga.nvim",
-    event = "LspAttach",
-    dependencies = { { "nvim-tree/nvim-web-devicons" }, { "nvim-treesitter/nvim-treesitter" } },
-    keys = {
-      { "\\\\", "<cmd>Lspsaga term_toggle<CR>", mode = { "n", "t" } },
-    },
-    config = function()
-      require("lspsaga").setup({})
-    end,
   },
 }

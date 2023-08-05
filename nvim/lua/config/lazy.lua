000c7778bee1f603a31488ec1a8eded8d52908a3
VyDucTan <vdt5snet@gmail.com>
2023-08-05 11:39:18 +0700

    feat(nvim): tailwindcss - update patern autocompletion, no need colorizer

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,7 +15,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" }, -- color highlight
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.json" },
     -- import/override with your plugins
     { import = "plugins" },
--- nvim/lua/plugins/lang/tailwindcss.lua
+++ nvim/lua/plugins/lang/tailwindcss.lua
@@ -5,24 +5,19 @@ return {
       servers = {
         tailwindcss = {
           settings = {
-            classAttributes = { "twMerge(" },
+            tailwindCSS = {
+              experimental = {
+                classRegex = {
+                  -- clsm
+                  { "clsm\\(([^)]*)\\)", "(?:'|\"|`)([^\"'`]*)(?:'|\"|`)" },
+                  -- headlessui transition
+                  "(?:enter|leave)(?:From|To)?=\\s*(?:\"|'|{`)([^(?:\"|'|`})]*)",
+                },
+              },
+            },
           },
         },
       },
     },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
-    },
-    opts = function(_, opts)
-      -- original LazyVim kind icon formatter
-      local format_kinds = opts.formatting.format
-      opts.formatting.format = function(entry, item)
-        format_kinds(entry, item) -- add icons
-        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-      end
-    end,
   },
 }
